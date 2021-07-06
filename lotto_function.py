52d1888794bb1d1e4bdd701d2aa8685a9af2afe0
kcw980729 <61902108+kcw980729@users.noreply.github.com>
2021-07-06 22:12:26 +0900

    Update lotto_function.py

--- lotto_function.py
+++ lotto_function.py
@@ -1,6 +1,16 @@
 import requests
 from bs4 import BeautifulSoup
 
+# get lotto number from user
+user_lotto = []
+print('Enter your lotto number')
+for i in range(7):
+    lotto_num = input()
+    user_lotto.append(lotto_num)
+    
+print(user_lotto)
+
+# scrap actual lotto number
 for i in range(965,971):
     url = 'https://search.naver.com/search.naver?sm=tab_drt&where=nexearch&query={}회로또'.format(i)
     req = requests.get(url)
